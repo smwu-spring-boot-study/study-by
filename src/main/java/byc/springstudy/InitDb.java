890e2ec7afc01828afd0d8adcd1080b385261b53
byunyc0124 <baeyonyk124@gmail.com>
2022-11-10 22:27:19 +0900

    feat: h2 -> postgres

--- src/main/java/byc/springstudy/InitDb.java
+++ src/main/java/byc/springstudy/InitDb.java
@@ -50,46 +50,50 @@ public class InitDb {
         insert into CATEGORY(DEPTH, TITLE, PARENT) values(2, '해외소설', 2);
         */
         public void categoryDBInit(){
-            Category category1 = Category.builder()
-                    .title("국내")
-                    .depth(1)
-                    .build();
-            Category category2 = Category.builder()
-                    .title("해외")
-                    .depth(1)
-                    .build();
-            categoryRepository.save(category1);
-            categoryRepository.save(category2);
+            List<Category> categoryList = categoryRepository.findAll();
+            if(categoryList.size() == 0){
 
-            //Category c1 = categoryRepository.findById(category1.getId());
-            List<Category> categoryList = categoryRepository.findAllCate();
-            Category c1 = categoryList.get(0);
-            Category c2 = categoryList.get(1);
+                Category category1 = Category.builder()
+                        .title("국내")
+                        .depth(1)
+                        .build();
+                Category category2 = Category.builder()
+                        .title("해외")
+                        .depth(1)
+                        .build();
+                categoryRepository.save(category1);
+                categoryRepository.save(category2);
 
-            Category category3 = Category.builder()
-                    .title("국내시")
-                    .depth(2)
-                    .parent(c1)
-                    .build();
-            Category category4 = Category.builder()
-                    .title("해외시")
-                    .depth(2)
-                    .parent(c2)
-                    .build();
-            Category category5 = Category.builder()
-                    .title("국내소설")
-                    .depth(2)
-                    .parent(c1)
-                    .build();
-            Category category6 = Category.builder()
-                    .title("해외소설")
-                    .depth(2)
-                    .parent(c2)
-                    .build();
-            categoryRepository.save(category3);
-            categoryRepository.save(category4);
-            categoryRepository.save(category5);
-            categoryRepository.save(category6);
+                //Category c1 = categoryRepository.findById(category1.getId());
+                List<Category> categories = categoryRepository.findAllCate();
+                Category c1 = categories.get(0);
+                Category c2 = categories.get(1);
+
+                Category category3 = Category.builder()
+                        .title("국내시")
+                        .depth(2)
+                        .parent(c1)
+                        .build();
+                Category category4 = Category.builder()
+                        .title("해외시")
+                        .depth(2)
+                        .parent(c2)
+                        .build();
+                Category category5 = Category.builder()
+                        .title("국내소설")
+                        .depth(2)
+                        .parent(c1)
+                        .build();
+                Category category6 = Category.builder()
+                        .title("해외소설")
+                        .depth(2)
+                        .parent(c2)
+                        .build();
+                categoryRepository.save(category3);
+                categoryRepository.save(category4);
+                categoryRepository.save(category5);
+                categoryRepository.save(category6);
+            }
         }
     }
 }
